698463c2fa6f8469ee872cc2561994a69976423e
Uğur Ethem AYDIN <ugur@metromedya.com>
2020-11-18 23:24:10 +0300

    iphone 12 mini
    iphone 12 mini screenSize issue fixed
    Battery detect feature added

--- Example/CheckDevice/ViewController.swift
+++ Example/CheckDevice/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
-
+import CheckDevice
 class ViewController: UIViewController {
 
+    @IBOutlet weak var battery: UILabel!
+    @IBOutlet weak var device: UILabel!
+    @IBOutlet weak var screenSize: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        screenSize.text = String(CheckDevice.type().rawValue)
+        device.text = CheckDevice.version().rawValue
+        battery.text = String(CheckDevice.battery.level)
+        dump(CheckDevice.version().rawValue)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
